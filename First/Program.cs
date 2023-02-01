ad3d60f2152903c7e7d71a423db5e95bae23cc44
Natalia <natasha.litvina@mail.ru>
2023-02-01 23:21:27 +0300

    create

---
+++ First/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+int NewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) NewArray++;
+}
+string[] NewArray1 = new string[NewArray];
+int count = 0;
+ 
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        NewArray1[count] = array[i];
+        count++;
+    }
+}
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(NewArray1);
+ 
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+} 
