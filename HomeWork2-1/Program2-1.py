351806314c2b20bb3c7558e328173977f84dc427
Natalycat86 <116194402+Natalycat86@users.noreply.github.com>
2023-02-03 11:40:01 +0300

    Update Program2-1.py

--- HomeWork2-1/Program2-1.py
+++ HomeWork2-1/Program2-1.py
@@ -5,8 +5,8 @@ A = int(input('Введите количество монет: '))
 from random import randint
 list = [randint(0, 1) for i in range (A)]
 print(list)
-while (b < A):
-    if 0 in list:
-        count += 1
-        b += 1
-print (count)
+reshka = list.count(1)
+orel = list.count(0)
+if reshka > orel:
+    print (orel)
+else: print (reshka)  
